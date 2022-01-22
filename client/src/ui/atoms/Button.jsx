f6c943b85e1da07c7bf83a826da8c1ba733e11a8
JagodaBodnar <jagodabodnar90@gmail.com>
2022-01-23 00:30:38 +0100

    Button icons added

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -3,7 +3,8 @@ import {Button as MuiButton} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-export function Button({children, ...props}) {
-    return <MuiButton {...props} startIcon={<AddIcon/>} endIcon={<ArrowForwardIosIcon/>} disableRipple
+export function Button({children, startIcon, endIcon, ...props}) {
+    return <MuiButton {...props}  disableRipple startIcon={startIcon && <AddIcon />} endIcon={endIcon && <ArrowForwardIosIcon />}
                       onClick={() => console.log("Button clicked")}>{children}</MuiButton>;
 }
+
--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -2,6 +2,8 @@ import noContent from "../../assets/no_content.png"
 import {styled} from '@mui/system';
 import {Box} from '@mui/material';
 import {theme} from "../../theme"
+import * as React from "react";
+import {Button} from "./Button";
 
 const StyledNoContentParagraph = styled('p')({
     color: `rgba(${theme.palette.text.secondary},0.5)`
