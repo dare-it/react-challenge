bd63852552e67540da7a6ec21619f56fdefd4aae
JagodaBodnar <jagodabodnar90@gmail.com>
2022-01-23 15:57:33 +0100

    Button styles verification

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -3,8 +3,8 @@ import {Button as MuiButton} from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-export function Button({children, startIcon, endIcon, ...props}) {
-    return <MuiButton {...props}  disableRipple startIcon={startIcon && <AddIcon />} endIcon={endIcon && <ArrowForwardIosIcon />}
-                      onClick={() => console.log("Button clicked")}>{children}</MuiButton>;
+export function Button({children,startIcon,text, endIcon, ...props}) {
+    return <MuiButton {...props}  disableRipple startIcon={startIcon && <AddIcon />} endIcon={endIcon && <ArrowForwardIosIcon/> }
+                      onClick={() => console.log("Button clicked")}><span style={{paddingRight: `${children.length === 0 ? '0px': endIcon ? '8px' : '0px'}`}}>{children}</span></MuiButton>;
 }
 
