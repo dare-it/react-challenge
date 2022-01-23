03a4f51367b76c7efc05dc5a979cdc7b00767e75
Jagoda Bodnar <49833192+JagodaBodnar@users.noreply.github.com>
2022-01-23 16:54:25 +0100

    Revert "Revert "Task 1""

--- client/src/ui/atoms/Error.jsx
+++ client/src/ui/atoms/Error.jsx
@@ -1,20 +1,28 @@
-import { Box, Typography } from '@mui/material';
+import {Box, Typography} from '@mui/material';
+import errorIcon from "../../assets/unknown_error.png"
+import {styled} from "@mui/system";
+import {theme} from "../../theme";
 
-export const Error = ({ error }) => {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      {
-        error?.message?.includes('Network Error') ? (
-          <Typography>Uruchom Server!</Typography>
-        ) : null //  TODO in TASK 1
-      }
-    </Box>
-  );
+const StyleErrorParagraph = styled('p')({
+    color: `rgba(${theme.palette.text.secondary},0.5)`
+
+})
+
+export const Error = ({error}) => {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+            }}
+        >
+            {
+                error?.message?.includes('Network Error') ? (
+                    <Typography>Uruchom Server!</Typography>
+                ) : <> <img src={errorIcon} alt="error icon"/><StyleErrorParagraph>Wystąpił nieoczekiwany błąd</StyleErrorParagraph></>
+            }
+        </Box>
+    );
 };
