3da3eb4183d4f0cc26628cf6588bfa8052b34214
Aleksandra Likus <likusaleksandra@gmail.com>
2022-02-01 22:03:40 +0100

    Dodanie i ostylowanie przycisków, dodanie alertów/powiadomien wraz ze zdjeciami i scentrowanie ich

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,3 +1,12 @@
+import no_content from '../../no_content.png';
+import { Box, Typography } from '@mui/material';
+
 export const NoContent = () => {
-  return null;
+  return (
+  <>
+  <Box >
+  <Typography>Brak danych do wyświetlenia </Typography>
+  <img src={no_content} />
+  </Box>
+  </>);
 };
