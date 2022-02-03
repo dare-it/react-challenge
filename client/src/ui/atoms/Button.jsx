ac512bf4a5dc8c6b2bcd41fab721786763db62a4
JagodaBodnar <jagodabodnar90@gmail.com>
2022-02-03 16:02:25 +0100

    Correction after feedback

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import {Button as MuiButton} from '@mui/material';
+import { Button as MuiButton } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-export function Button({children,startIcon,text, endIcon, ...props}) {
-    return <MuiButton {...props}  disableRipple startIcon={startIcon && <AddIcon />} endIcon={endIcon && <ArrowForwardIosIcon/> }
-                      onClick={() => console.log("Button clicked")}><span style={{paddingRight: `${children.length === 0 ? '0px': endIcon ? '8px' : '0px'}`}}>{children}</span></MuiButton>;
+export function Button({ children, startIcon, text, endIcon, ...props }) {
+  return <MuiButton {...props} disableRipple startIcon={startIcon && <AddIcon />}
+                    endIcon={endIcon && <ArrowForwardIosIcon />}
+                    onClick={props.onClick}><span
+    style={{ paddingRight: `${children.length === 0 ? '0px' : endIcon ? '8px' : '0px'}` }}>{children}</span></MuiButton>;
 }
 
