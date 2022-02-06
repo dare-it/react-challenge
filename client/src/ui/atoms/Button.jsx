813adc4498ffa71c4da51688be43026d34971424
olawwska <olwojciech@gmail.com>
2022-02-06 20:41:22 +0100

    fix: destrukturyzacja props

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -1,24 +1,6 @@
 import * as React from 'react';
 import { Button as MuiButton } from '@mui/material';
-import * as PropTypes from 'prop-types';
 
-export function Button({ children, color, variant, disabled, onClick }) {
-  return (
-    <MuiButton
-      color={color}
-      variant={variant}
-      disabled={false}
-      onClick={onClick}
-    >
-      {children}
-    </MuiButton>
-  );
+export function Button({ props, children }) {
+  return <MuiButton {...props}>{children}</MuiButton>;
 }
-
-Button.propTypes = {
-  children: PropTypes.node,
-  color: PropTypes.string,
-  variant: PropTypes.string,
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-};
