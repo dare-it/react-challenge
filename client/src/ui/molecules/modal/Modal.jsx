7bb3152a522ce4c27f9935182212affc2f2876a9
Martyna <m.smolarek19@gmail.com>
2022-02-26 17:22:52 +0100

    added modal styling

--- client/src/ui/molecules/modal/Modal.jsx
+++ client/src/ui/molecules/modal/Modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Box } from '@mui/material'
+import { Typography, Box, Card, CardContent, CardActions } from '@mui/material'
 import { Button } from 'ui/atoms/Button'
 
 const modalStyling = {
@@ -13,33 +13,43 @@ const modalStyling = {
     borderRadius: '8px',
     boxShadow: 20,
     p: 4,
+}
+
+const boxStyling = {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-evenly',
+}
 
+const cardActionStyling = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
 }
 
 const modalButtonsStyling = {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'flex-end',
+    p: 2
 }
 
 const MoneyManageModal = ({ title, description, handleClose, handleSubmit, ...props }) => {
 
     return (
+        <Card sx={modalStyling} >
+            <CardContent sx={boxStyling}>
+                <Typography variant={'h3'}>{title}</Typography>
+                <Typography variant={'h4'}>{description}</Typography>
+                <CardActions sx={cardActionStyling}>
+                    <Box sx={modalButtonsStyling} >
+                        <Button variant={'outlined'} onClick={handleClose} sx={{ m: 1 }}>Anuluj</Button>
+                        <Button variant={'contained'} onClick={handleSubmit} sx={{ m: 1 }}>Zapisz</Button>
+                    </Box>
+                </CardActions>
 
-        <Box sx={modalStyling} >
-
-            <Typography variant={'h3'}>{title}</Typography>
-
-            <Typography variant={'h4'}>{description}</Typography>
-            <Box sx={modalButtonsStyling} >
-                <Button variant={'outlined'} onClick={handleClose} sx={{ m: 1 }}>Anuluj</Button>
-                <Button variant={'contained'} onClick={handleSubmit} sx={{ m: 1 }}>Zapisz</Button>
-            </Box>
-        </Box>
-        // </Modal >
+            </CardContent>
+        </Card>
     )
 }
 
