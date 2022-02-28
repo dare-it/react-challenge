5c433013199f91d99b82c904db73a59499835f08
tdxa <anndoman3@gmail.com>
2022-02-28 21:38:01 +0100

    Add delete functionality

--- client/src/ui/organisms/BudgetTable.jsx
+++ client/src/ui/organisms/BudgetTable.jsx
@@ -1,18 +1,11 @@
 import { BUDGET_QUERY } from 'queryKeys';
 import React from 'react';
-import { useQuery, useQueryClient } from 'react-query';
+import { BudgetService } from 'api';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { CategoryCell, Error, Loader, LocalizedDate, Money, NoContent } from 'ui';
 import { Table } from 'ui/molecules/table/Table';
 
-const tableDefinition = [
-  { id: 'name', label: 'Nazwa', renderCell: (row) => (
-    <CategoryCell color={row.category?.color} name={row.category?.name} />
-  ),},
-  { id: 'plannedExpenses', label: 'Planowane wydatki', renderCell: (row) => <Money inCents={row.amountInCents} /> },
-  { id: 'currentAmount', label: 'Obecna kwota', renderCell: (row) => <Money inCents={row.currentSpending} /> },
-  { id: 'status', label: 'Status' },
-  { id: 'createdDate', label: 'Data utworzenia' ,  renderCell: (row) => <LocalizedDate date={row.createdAt} />},
-];
+
 
 const BudgetTable = () => {
   const queryClient = useQueryClient();
@@ -20,6 +13,14 @@ const BudgetTable = () => {
   const { isLoading, error, data } = useQuery(BUDGET_QUERY, () =>
     BudgetService.findAll(),
   );
+
+  const mutation = useMutation((ids) => BudgetService.remove({ ids }), {
+    onSuccess: async () => {
+      await queryClient.refetchQueries([BUDGET_QUERY]);
+    },
+  });
+
+  const deleteRows = (ids) => mutation.mutate(ids);
 
   if (isLoading) {
     return <Loader />;
@@ -34,7 +35,22 @@ const BudgetTable = () => {
     return <NoContent />
   }
 
-  return <Table rows={data} getUniqueId={(row) => row.id} headCells={tableDefinition} />;
+
+  const tableDefinition = [
+    { id: 'name', label: 'Nazwa', renderCell: (row) => (
+      <CategoryCell color={row.category?.color} name={row.category?.name} />
+    ),},
+    { id: 'plannedExpenses', label: 'Planowane wydatki', renderCell: (row) => <Money inCents={row.amountInCents} /> },
+    { id: 'currentAmount', label: 'Obecna kwota', renderCell: (row) => <Money inCents={row.currentSpending} /> },
+    { id: 'status', label: 'Status',  renderCell: (row) => {
+      if (row.currentSpending === row.amountInCents) return 'Wykorzystany';
+      if (row.currentSpending > row.amountInCents) return 'Przekroczone';
+      if (row.currentSpending < row.amountInCents) return 'W normie';
+    }, },
+    { id: 'createdDate', label: 'Data utworzenia' ,  renderCell: (row) => <LocalizedDate date={row.createdAt} />},
+  ];
+
+  return <Table rows={data} getUniqueId={(row) => row.id} headCells={tableDefinition} deleteRecords={deleteRows} />;
 };
 
 export default BudgetTable;
