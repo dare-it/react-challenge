613fea7999f35b3f0b05b378f4622ddc63cadc3e
tdxa <anndoman3@gmail.com>
2022-03-02 00:55:30 +0100

    Add Ledger modal

--- client/src/ui/organisms/Ledger.widget.jsx
+++ client/src/ui/organisms/Ledger.widget.jsx
@@ -1,5 +1,5 @@
-import { Box, Grid } from '@mui/material';
-import React from 'react';
+import { Box } from '@mui/material';
+import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
 import {
@@ -18,9 +18,20 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { LEDGER_QUERY } from 'queryKeys';
 import { LedgerService } from 'api';
 import { theme } from '../../theme';
+import AddNewLedgerRecordModal from './modals/AddNewLedgerRecord.modal';
 
 export const LedgerWidget = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState('INCOME');
   const queryClient = useQueryClient();
+
+  const handleModalOpen = (modalType) => {
+    setModalType(modalType);
+    setIsModalOpen(true);
+  };
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
 
   const { isLoading, error, data } = useQuery(LEDGER_QUERY, () =>
     LedgerService.findAll(),
@@ -53,7 +64,7 @@ export const LedgerWidget = () => {
       label: 'Kwota',
       renderCell: (row) =>
         row.mode === 'INCOME' ? (
-          <Box sx={{ color: theme.palette.success.dark}}>
+          <Box sx={{ color: theme.palette.success.dark }}>
             +<Money inCents={row.amountInCents} />
           </Box>
         ) : (
@@ -65,39 +76,52 @@ export const LedgerWidget = () => {
   ];
 
   return (
-    <Card
-      title={
-        <ActionHeader
-          variant={'h1'}
-          title="Portfel"
-          renderActions={() => (
-            <Box>
-              <Button text="Wpłać" variant="outlined" startIcon={<AddIcon />} />
-              <Button
-                text="Wypłać"
-                variant="outlined"
-                sx={{ marginLeft: '15px' }}
-                startIcon={<RemoveIcon />}
-              />
-            </Box>
-          )}
-        />
-      }
-    >
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Error />
-      ) : !data.length ? (
-        <NoContent />
-      ) : (
-        <Table
-          headCells={tableDefinition}
-          rows={data}
-          getUniqueId={(row) => row.id}
-          deleteRecords={deleteRows}
-        />
-      )}
-    </Card>
+    <>
+      <AddNewLedgerRecordModal
+        open={isModalOpen}
+        handleClose={handleModalClose}
+        type={modalType}
+      />
+      <Card
+        title={
+          <ActionHeader
+            variant={'h1'}
+            title="Portfel"
+            renderActions={() => (
+              <Box>
+                <Button
+                  text="Wpłać"
+                  variant="outlined"
+                  startIcon={<AddIcon />}
+                  onClick={()=> handleModalOpen('INCOME')}
+                />
+                <Button
+                  text="Wypłać"
+                  variant="outlined"
+                  sx={{ marginLeft: '15px' }}
+                  startIcon={<RemoveIcon />}
+                  onClick={()=> handleModalOpen('EXPENSE')}
+                />
+              </Box>
+            )}
+          />
+        }
+      >
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Error />
+        ) : !data.length ? (
+          <NoContent />
+        ) : (
+          <Table
+            headCells={tableDefinition}
+            rows={data}
+            getUniqueId={(row) => row.id}
+            deleteRecords={deleteRows}
+          />
+        )}
+      </Card>
+    </>
   );
 };
