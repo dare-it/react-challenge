926fc71399a901cd9c1d630cce0dd7ce87d5893b
tdxa <anndoman3@gmail.com>
2022-03-01 21:37:27 +0100

    Create Ledger page

--- client/src/ui/organisms/Ledger.widget.jsx
+++ client/src/ui/organisms/Ledger.widget.jsx
@@ -2,15 +2,67 @@ import { Grid } from '@mui/material';
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { ActionHeader, Button, Card, Table } from 'ui';
+import {
+  ActionHeader,
+  Button,
+  Card,
+  CategoryCell,
+  Error,
+  Loader,
+  LocalizedDate,
+  Money,
+  NoContent,
+} from 'ui';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { LEDGER_QUERY } from 'queryKeys';
+import { LedgerService } from 'api';
+import { Table } from 'ui/molecules/table/Table';
 
 export const LedgerWidget = () => {
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery(LEDGER_QUERY, () =>
+    LedgerService.findAll(),
+  );
+
+  const mutation = useMutation((ids) => LedgerService.remove({ ids }), {
+    onSuccess: async () => {
+      await queryClient.refetchQueries([LEDGER_QUERY]);
+    },
+  });
+
+  const deleteRows = (ids) => mutation.mutate(ids);
+
   const tableDefinition = [
-    { id: 'name', label: 'Nazwa', renderCell: (row) => {} },
-    { id: 'category', label: 'Kategoria', renderCell: (row) => {} },
-    { id: 'date', label: 'Data', renderCell: (row) => {} },
-    { id: 'amount', label: 'Kwota', renderCell: (row) => {} },
+    { id: 'name', label: 'Nazwa', renderCell: (row) => row.title },
+    {
+      id: 'category',
+      label: 'Kategoria',
+      renderCell: (row) => <CategoryCell name={row.category?.name} />,
+    },
+    {
+      id: 'date',
+      label: 'Data',
+      renderCell: (row) => <LocalizedDate date={row.createdAt} />,
+    },
+    {
+      id: 'amount',
+      label: 'Kwota',
+      renderCell: (row) => <Money inCents={row.amountInCents} />,
+    },
   ];
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <Error error={error} />;
+  }
+
+  if (!data?.length) {
+    return <NoContent />;
+  }
 
   return (
     <Card
@@ -31,7 +83,12 @@ export const LedgerWidget = () => {
         >
           <Grid container>
             <Grid item xs={12}>
-              <Table headCells={tableDefinition}></Table>
+              <Table
+                rows={data}
+                getUniqueId={(row) => row.id}
+                headCells={tableDefinition}
+                deleteRecords={deleteRows}
+              />
             </Grid>
           </Grid>
         </ActionHeader>
